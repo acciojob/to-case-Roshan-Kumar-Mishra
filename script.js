d8ab562abc458b9e41073ceb5f13cd6ac7ae689e
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-13 18:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function toCase(text) {
-  // write your code here
+function toCase(inputString) {
+    // Check if the input string is empty
+    if (!inputString) {
+        return "";
+    }
+
+    // Convert the string to lowercase and uppercase
+    const lowercaseString = inputString.toLowerCase();
+    const uppercaseString = inputString.toUpperCase();
+
+    // Combine the lowercase and uppercase strings with a hyphen as the delimiter
+    const resultString = lowercaseString + '-' + uppercaseString;
+
+    return resultString;
 }
 
-// DO not change the code below
+// Example usage:
+// const inputString = "HelloWorld";
+// const result = toCase(inputString);
+// console.log(result);
 
-const text = prompt("Enter text:");
-alert(toCase(text));
